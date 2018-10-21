45a4b4e47e32eccb2db01746467fd53ab7abf3d3
davidhorm <davidhorm+github@gmail.com>
2018-10-20 19:16:59 -0700

    feat: adding Divider layout

---
+++ src/app/divider/divider.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divider',
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.css']
+})
+export class DividerComponent implements OnInit {
+  title = "TODO Title";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
