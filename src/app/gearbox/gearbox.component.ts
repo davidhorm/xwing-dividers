a44a3752aa5358e85ddc57468fa4b74ffeb80065
davidhorm <davidhorm+github@gmail.com>
2018-11-04 17:27:49 -0800

    style: rotating tuckbox

--- src/app/gearbox/gearbox.component.ts
+++ src/app/gearbox/gearbox.component.ts
@@ -10,13 +10,23 @@ import { GEARBOX_SIZE } from './gearbox-size.service';
 export class GearboxComponent implements OnInit {
 
   selectedGearboxSize: GearboxSize;
-  defaultDepth: Number = 16;
-  shipTokenDepth: Number = 2;
-  depth: Number;
+  defaultDepth: number = 16;
+  shipTokenDepth: number = 2;
+  depth: number;
 
   constructor() { 
     this.selectedGearboxSize = GEARBOX_SIZE[0];
     this.depth = this.defaultDepth;
+  }
+
+  rotateTable() {
+    let newHeight = this.depth*3 + this.selectedGearboxSize.height;
+
+    return {
+      'transform': `rotate(90deg) translateX(-${newHeight}mm)`,
+      'transform-origin': 'left bottom',
+      'height': `${newHeight}mm`
+    };
   }
 
   ngOnInit() {
