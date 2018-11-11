68d42e341e5af6c3e9acd1560cbea399f4987c64
davidhorm <davidhorm+github@gmail.com>
2018-11-10 19:52:11 -0800

    refactor: side component to take input

--- src/app/gearbox/side/side.component.ts
+++ src/app/gearbox/side/side.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-side',
@@ -9,6 +9,37 @@ export class SideComponent implements OnInit {
 
   constructor() { }
 
+  @Input() shipName:string;
+  @Input() side:string;
+  @Input() boxDepth:number;
+  @Input() sideWidth:number;
+
+  getSideStyle() {
+    let padding = 3;
+
+    return {
+      'padding': `${padding}mm`,
+      'height': `${this.boxDepth - (padding * 2)}mm`,
+      'width': `${this.sideWidth - (padding * 2)}mm`,
+      'transform': `${this.getTransform()}`
+    };
+  }
+
+  getTransform() {
+    if(this.side === 'top') {
+      return "rotate(180deg) translate(-100%, -50%);";
+    }
+    else if (this.side === 'left') {
+      return "rotate(90deg) translate(-50%, -100%);";
+    }
+    else if (this.side === 'right') {
+      return "rotate(-90deg) translate(-50%);";
+    }
+    else if (this.side === 'bottom') {
+      return "rotate(0deg) translate(0, -50%);";
+    }
+  }
+  
   ngOnInit() {
   }
 
