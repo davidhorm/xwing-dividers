73c87aeae975bd812b498f395f2306b2b68a349c
davidhorm <davidhorm+github@gmail.com>
2018-11-10 22:58:47 -0800

    refactor: implementing SideComponent

--- src/app/gearbox/side/side.component.ts
+++ src/app/gearbox/side/side.component.ts
@@ -10,33 +10,39 @@ export class SideComponent implements OnInit {
   constructor() { }
 
   @Input() shipName:string;
-  @Input() side:string;
+  @Input() boxSide:string;
   @Input() boxDepth:number;
   @Input() sideWidth:number;
 
   getSideStyle() {
     let padding = 3;
+    let transformStyle = this.getTransform();
 
     return {
       'padding': `${padding}mm`,
       'height': `${this.boxDepth - (padding * 2)}mm`,
       'width': `${this.sideWidth - (padding * 2)}mm`,
-      'transform': `${this.getTransform()}`
+      'transform-origin': 'top left',
+      'transform': `${transformStyle}`,
+      'position': 'absolute'
     };
   }
 
-  getTransform() {
-    if(this.side === 'top') {
-      return "rotate(180deg) translate(-100%, -50%);";
+  getTransform():string {
+    if(this.boxSide === 'top') {
+      return "rotate(180deg) translate(-100%, -50%)";
     }
-    else if (this.side === 'left') {
-      return "rotate(90deg) translate(-50%, -100%);";
+    else if (this.boxSide === 'left') {
+      return "rotate(90deg) translate(-50%, -100%)";
     }
-    else if (this.side === 'right') {
-      return "rotate(-90deg) translate(-50%);";
+    else if (this.boxSide === 'right') {
+      return "rotate(-90deg) translate(-50%)";
     }
-    else if (this.side === 'bottom') {
-      return "rotate(0deg) translate(0, -50%);";
+    else if (this.boxSide === 'bottom') {
+      return "rotate(0deg) translate(0, -50%)";
+    }
+    else {
+      return null;
     }
   }
   
