68bbe11f97a3cae738e7b9d8774510974cca2688
davidhorm <davidhorm+github@gmail.com>
2018-11-29 11:36:23 -0800

    style: swapped width/height of Medium and Large to reduce overall height of box.

--- src/app/gearbox/shared/gearbox-size.service.ts
+++ src/app/gearbox/shared/gearbox-size.service.ts
@@ -2,6 +2,6 @@ import { GearboxSize } from "./gearbox-size.model";
 
 export const GEARBOX_SIZE: GearboxSize[] = [
     {name:"Small", width:51, height:48},
-    {name:"Medium", width:61.5, height:64.5},
-    {name:"Large", width:82, height:84}
+    {name:"Medium", width:64.5, height:61.5},
+    {name:"Large", width:84, height:82}
 ];
