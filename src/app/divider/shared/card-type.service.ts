3bd3161924c93245cf4420f5cfd2f2d21488cd92
davidhorm <davidhorm+github@gmail.com>
2018-12-10 16:43:07 -0800

    feat: added xwing to tie fighter to Card Type dropdown

--- src/app/divider/shared/card-type.service.ts
+++ src/app/divider/shared/card-type.service.ts
@@ -1,30 +1,32 @@
 import { CardType } from './card-type.model';
 
 export const UpgradeCardTypes: CardType[] = [
-    { title: 'Astromech',   iconClassName:'xwing-miniatures-font-astromech',    defaultLength: 8 },
-    { title: 'Cannon',      iconClassName:'xwing-miniatures-font-cannon',       defaultLength: 8 },
-    { title: 'Configuration',iconClassName:'xwing-miniatures-font-config',      defaultLength: 4 },
-    { title: 'Crew',        iconClassName:'xwing-miniatures-font-crew',         defaultLength: 27 },
-    { title: 'Device',      iconClassName:'xwing-miniatures-font-device',       defaultLength: 10 },
-    { title: 'Force Power', iconClassName:'xwing-miniatures-font-forcepower',   defaultLength: 3 },
-    { title: 'Gunner',      iconClassName:'xwing-miniatures-font-gunner',       defaultLength: 11 },
-    { title: 'Illicit',     iconClassName:'xwing-miniatures-font-illicit',      defaultLength: 8 },
-    { title: 'Missile',     iconClassName:'xwing-miniatures-font-missile',      defaultLength: 13 },
-    { title: 'Modification',iconClassName:'xwing-miniatures-font-modification', defaultLength: 19 },
-    { title: 'Sensor',      iconClassName:'xwing-miniatures-font-sensor',       defaultLength: 7 },
-    { title: 'Talent',      iconClassName:'xwing-miniatures-font-talent',       defaultLength: 33 },
-    { title: 'Tech',        iconClassName:'xwing-miniatures-font-tech',         defaultLength: 5 },
-    { title: 'Title',       iconClassName:'xwing-miniatures-font-title',        defaultLength: 7 },
-    { title: 'Torpedo',     iconClassName:'xwing-miniatures-font-torpedo',      defaultLength: 9 },
-    { title: 'Turret',      iconClassName:'xwing-miniatures-font-turret',       defaultLength: 5 }
+    { title: 'Astromech',   iconClassName:'xwing-miniatures-font xwing-miniatures-font-astromech',      defaultLength: 8 },
+    { title: 'Cannon',      iconClassName:'xwing-miniatures-font xwing-miniatures-font-cannon',         defaultLength: 8 },
+    { title: 'Configuration',iconClassName:'xwing-miniatures-font xwing-miniatures-font-config',        defaultLength: 4 },
+    { title: 'Crew',        iconClassName:'xwing-miniatures-font xwing-miniatures-font-crew',           defaultLength: 27 },
+    { title: 'Device',      iconClassName:'xwing-miniatures-font xwing-miniatures-font-device',         defaultLength: 10 },
+    { title: 'Force Power', iconClassName:'xwing-miniatures-font xwing-miniatures-font-forcepower',     defaultLength: 3 },
+    { title: 'Gunner',      iconClassName:'xwing-miniatures-font xwing-miniatures-font-gunner',         defaultLength: 11 },
+    { title: 'Illicit',     iconClassName:'xwing-miniatures-font xwing-miniatures-font-illicit',        defaultLength: 8 },
+    { title: 'Missile',     iconClassName:'xwing-miniatures-font xwing-miniatures-font-missile',        defaultLength: 13 },
+    { title: 'Modification',iconClassName:'xwing-miniatures-font xwing-miniatures-font-modification',   defaultLength: 19 },
+    { title: 'Sensor',      iconClassName:'xwing-miniatures-font xwing-miniatures-font-sensor',         defaultLength: 7 },
+    { title: 'Talent',      iconClassName:'xwing-miniatures-font xwing-miniatures-font-talent',         defaultLength: 33 },
+    { title: 'Tech',        iconClassName:'xwing-miniatures-font xwing-miniatures-font-tech',           defaultLength: 5 },
+    { title: 'Title',       iconClassName:'xwing-miniatures-font xwing-miniatures-font-title',          defaultLength: 7 },
+    { title: 'Torpedo',     iconClassName:'xwing-miniatures-font xwing-miniatures-font-torpedo',        defaultLength: 9 },
+    { title: 'Turret',      iconClassName:'xwing-miniatures-font xwing-miniatures-font-turret',         defaultLength: 5 },
+    { title: 'T-65 X-wing', iconClassName:'xwing-miniatures-ship xwing-miniatures-ship-t65xwing',       defaultLength: 7 },
+    { title: 'TIE/ln Fighter',iconClassName:'xwing-miniatures-ship xwing-miniatures-ship-tielnfighter', defaultLength: 10 }
 ];
 
 export const FactionTypes: CardType[] = [
-    { title: 'First Order',         iconClassName:'xwing-miniatures-font-firstorder',   defaultLength: 20 },
-    { title: 'Galactic Empire',     iconClassName:'xwing-miniatures-font-empire',       defaultLength: 40 },
-    { title: 'Galactic Republic',   iconClassName:'xwing-miniatures-font-republic',     defaultLength: 20 },
-    { title: 'Rebel Alliance',      iconClassName:'xwing-miniatures-font-rebel',        defaultLength: 40 },
-    { title: 'Resistance',          iconClassName:'xwing-miniatures-font-rebel-outline',defaultLength: 20 },
-    { title: 'Scum & Villainy',     iconClassName:'xwing-miniatures-font-scum',         defaultLength: 40 },
-    { title: 'Separatist Alliance', iconClassName:'xwing-miniatures-font-separatists',  defaultLength: 20 }
+    { title: 'First Order',         iconClassName:'xwing-miniatures-font xwing-miniatures-font-firstorder',   defaultLength: 20 },
+    { title: 'Galactic Empire',     iconClassName:'xwing-miniatures-font xwing-miniatures-font-empire',       defaultLength: 40 },
+    { title: 'Galactic Republic',   iconClassName:'xwing-miniatures-font xwing-miniatures-font-republic',     defaultLength: 20 },
+    { title: 'Rebel Alliance',      iconClassName:'xwing-miniatures-font xwing-miniatures-font-rebel',        defaultLength: 40 },
+    { title: 'Resistance',          iconClassName:'xwing-miniatures-font xwing-miniatures-font-rebel-outline',defaultLength: 20 },
+    { title: 'Scum & Villainy',     iconClassName:'xwing-miniatures-font xwing-miniatures-font-scum',         defaultLength: 40 },
+    { title: 'Separatist Alliance', iconClassName:'xwing-miniatures-font xwing-miniatures-font-separatists',  defaultLength: 20 }
 ];
